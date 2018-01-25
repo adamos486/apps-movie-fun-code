1d2d4bc7ec147bf40c972feaaf6c6716b6cd5ec9
adamos486 <adamos486@gmail.com>
2018-01-25 13:40:16 -0800

    Magic hibernate gremlins

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -2,44 +2,41 @@ package org.superbiz.moviefun;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CsvUtils {
 
-    public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+  public static String readFile(String path) {
+    ClassLoader loader = CsvUtils.class.getClassLoader();
+    InputStream resourceAsStream = loader.getResourceAsStream(path);
+    Scanner scan = new Scanner(resourceAsStream).useDelimiter("\\A");
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    //Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+    //
+    if (scan.hasNext()) {
+      return scan.next();
+    } else {
+      return "";
     }
+  }
 
-    public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
-        try {
-            List<T> results = new ArrayList<>();
+  public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
+    try {
+      List<T> results = new ArrayList<>();
 
-            MappingIterator<T> iterator = objectReader.readValues(readFile(path));
+      MappingIterator<T> iterator = objectReader.readValues(readFile(path));
 
-            while (iterator.hasNext()) {
-                results.add(iterator.nextValue());
-            }
+      while (iterator.hasNext()) {
+        results.add(iterator.nextValue());
+      }
 
-            return results;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+      return results;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
--- src/main/java/org/superbiz/moviefun/albums/AlbumFixtures.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumFixtures.java
@@ -3,30 +3,28 @@ package org.superbiz.moviefun.albums;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
+import org.springframework.stereotype.Component;
 
 import static com.fasterxml.jackson.dataformat.csv.CsvSchema.ColumnType.NUMBER;
 import static org.superbiz.moviefun.CsvUtils.readFromCsv;
 
-@Component
-public class AlbumFixtures {
+@Component public class AlbumFixtures {
 
-    private final ObjectReader objectReader;
+  private final ObjectReader objectReader;
 
-    public AlbumFixtures() {
-        CsvSchema schema = CsvSchema.builder()
-            .addColumn("artist")
-            .addColumn("title")
-            .addColumn("year", NUMBER)
-            .addColumn("rating", NUMBER)
-            .build();
+  public AlbumFixtures() {
+    CsvSchema schema = CsvSchema.builder()
+        .addColumn("artist")
+        .addColumn("title")
+        .addColumn("year", NUMBER)
+        .addColumn("rating", NUMBER)
+        .build();
 
-        objectReader = new CsvMapper().readerFor(Album.class).with(schema);
-    }
+    objectReader = new CsvMapper().readerFor(Album.class).with(schema);
+  }
 
-    public List<Album> load() {
-        return readFromCsv(objectReader, "album-fixtures.csv");
-    }
+  public List<Album> load() {
+    return readFromCsv(objectReader, "album-fixtures.csv");
+  }
 }
